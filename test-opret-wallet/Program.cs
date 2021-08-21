e5dbd71641b85cb3850b5af6d60fd7a8ce7fa851
Daniel Florescu <df@z5.lt>
2021-08-21 13:43:57 +0200

    test-opret-wallet: Write and test example code.

---
+++ test-opret-wallet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+using System.Diagnostics;
+
+using Nethereum.Web3;
+using Nethereum.Signer;
+using Nethereum.Web3.Accounts;
+
+namespace test_opret_wallet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* BNB testnet id */
+            var chainId = 97;
+            /* Keep track of time used by generating new key. */
+            var sw = new Stopwatch();
+
+            sw.Start();
+            var account = generateAccount(chainId);
+            sw.Stop();
+
+            Console.WriteLine($"Took {sw.ElapsedMilliseconds}ms to generate new account");
+
+            printAccountInfo(account);
+        }
+
+        static void printAccountInfo(Account account) {
+            Console.WriteLine($"Public key {account.PublicKey}");
+            Console.WriteLine($"Private key {account.PrivateKey}");
+        }
+
+        static Account generateAccount(BigInteger chainId) {
+            /* Generate EC private and public keys. */
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+
+            /* Return new account with the newly generated keys. */
+            return new Account(ecKey, chainId);
+        }
+    }
+}
